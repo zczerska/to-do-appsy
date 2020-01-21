97da864cd966994a323d054a5b5eb4eb73b8114a
zczerska <zofia.hello@gmail.com>
2020-01-21 12:51:41 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
- function onReady() {
-   const addToDoForm = document.getElementById('addToDoForm');
+function onReady() {
+ const addToDoForm = document.getElementById('addToDoForm');
    const newToDoText = document.getElementById('newToDoText');
    const toDoList = document.getElementById('toDoList');
-	 addToDoForm.addEventListener('submit', event => { 
-		 event.preventDefault();
-		  });
-	 // get the text
+	addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+		
+		 // get the text
     let title = newToDoText.value;
-	  // create a new li
+		// create a new li
     let newLi = document.createElement('li');
 
     // create a new input
@@ -15,18 +15,21 @@
 
     // set the input's type to checkbox
     checkbox.type = "checkbox";
-	 // set the title
+		
+		// set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
-	 // attach the li to the ul
+		
+		// attach the li to the ul
     toDoList.appendChild(newLi);
-	 //empty the input
+		
+		 //empty the input
     newToDoText.value = '';
- } 
 
+  });
+ }
 window.onload = function() {
-   onReady();
+  onReady(); 
  };
- 
